2071d0c14543bf0629030623914f94c2843e81df
Kutay Akpinar <kutayakpnar@gmail.com>
2023-08-11 17:15:55 +0300

    TernaryOperators

---
+++ ternaryOperators.js
@@ -0,0 +1,12 @@
+// three statement 
+// condition ? ifTrue:ifFalse
+
+const isOpen="we dont know";
+
+isOpen==="true" ? console.log("You can pass") : console.log("You cannot pass");
+
+isOpen==="true" ? console.log("You can pass") : isOpen==="false" ? console.log("You cannot pass"):console.log("We dont know");
+
+
+
+
